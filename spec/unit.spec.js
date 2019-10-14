efdbeb07dbb762cfd6b1880cf7648698fb79ca2f
Madison Ehlers <madisonehlers@Madisons-MacBook-Pro.local>
2019-10-13 22:33:05 -0500

    Adding some additional tests

--- spec/unit.spec.js
+++ spec/unit.spec.js
@@ -24,8 +24,24 @@ describe('is 10', () => {
 });
 
 describe('A value that is not even a number', () => {
-    it('should return false', () => {
+    it('should return false for Infinity', () => {
         expect(isTen(Infinity)).toEqual(false);
+    });
+
+    it('should return false for a function', () => {
+        expect(isTen(() => {})).toEqual(false);
+    });
+
+    it('should return false for null', () => {
+        expect(isTen(null)).toEqual(false);
+    });
+
+    it('should return false for an array of singleton 10', () => {
+        expect(isTen([10])).toEqual(false);
+    });
+
+    it('should return false for an array of 10 items', () => {
+        expect(isTen([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual(false);
     });
 });
 
